52d953c7cc23be0cb24dbbd76475b8536be7978f
NateMDuck <111247182+NateMDuck@users.noreply.github.com>
2023-12-21 18:39:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import streamlit as st
-
 import random
 import time
 
 from gtts import gTTS
-from io import BytesIO
-sound_file = BytesIO()
+import os
 
-list_statistics = []
+list_side = [' right', ' left']
 
 list_moves =[' shift', ' double shift', ' triple shift',
             ' roll dodge', ' question mark dodge',
@@ -15,49 +12,43 @@ list_moves =[' shift', ' double shift', ' triple shift',
 
 list_shots = [' high', ' side', ' low']
 
-list_goal = [' upper ninety', ' lower ninety', ' nutmeg']
+list_goal = [' right upper ninety', ' left upper ninety',
+             ' right lower ninety', ' left lower ninety', ' nutmeg']
 
-list_side = [' right', ' left']
 
-num_rounds = int(st.number_input("rounds"))
+
+list_statistics = []
+
+#move = random.choice(list_moves)
+
+def text_to_speech(text):
+    # Initialize gTTS with the text to convert
+    speech = gTTS(text)
+
+    # Save the audio file to a temporary file
+    speech_file = 'speech.mp3'
+    speech.save(speech_file)
+
+    # Play the audio file
+    os.system('afplay ' + speech_file)
+
+num_rounds = int(input("Enter a number"))
 
 for i in range(num_rounds):
 
     move = random.choice(list_side)
-
-    if move != "nutmeg":
-        move = move + " " + random.choice(list_moves)
+    move = move + " " + random.choice(list_moves)
+    text_to_speech(move)
 
     list_statistics.append(move)
 
-    os.system(f'say{move}')
-
     time.sleep(1.0)
 
 
-    shot = random.choice(list_side) + " " + random.choice(list_shots)
-    shot = shot + " " + random.choice(list_goal) + " " + random.choice(list_side)
+    shot = random.choice(list_side)
+    shot = shot + " " + random.choice(list_shots) + " " + random.choice(list_goal)
+    text_to_speech(shot)
 
     list_statistics.append(shot)
 
-    os.system(f'say{shot}')
-
-
-    time.sleep(1.0)
-
-tts = gTTS('Add text-to-speech to your app', lang='en')
-tts.write_to_fp(sound_file)
-st.audio(sound_file)
-
-chunk_size = 2
-
-# Example 1 : Using list slicing
-def chunkify(list_statistics, chunk_size):
-    return [list_statistics[i:i+chunk_size] for i in range(0, len(list_statistics), chunk_size)]
-
-result = chunkify(list_statistics, chunk_size)
-print(result)
-
-
-
-
+print(list_statistics)
